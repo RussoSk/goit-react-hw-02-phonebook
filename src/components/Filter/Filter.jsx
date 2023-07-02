1025cfd3cd742efd696d027e37b9ccd11f261866
RussoSk <117772437+RussoSk@users.noreply.github.com>
2023-07-02 21:37:12 +0300

    2.1
    test debag

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './Filter.module.css'
+
 export class Filter extends Component {
     render() {
       const { filter, onChange } = this.props;
